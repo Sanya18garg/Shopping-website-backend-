83d85b7c098a00f508e6e505276660f534140f45
sanya18garg <1976rajnigarg@gmail.com>
2019-10-17 17:23:47 +0530

    6 commit

--- src/main/java/com/caseStudy/eCart/controller/productsController.java
+++ src/main/java/com/caseStudy/eCart/controller/productsController.java
@@ -44,6 +44,11 @@ public List<Products> getAllNotes()
     return p.findAllByProductPriceBetween(p_price1,p_price2);
 
 }
+@PostMapping("/create")
+    public Products creater(@Valid @RequestBody Products u)
+{
+    return p.save(u);
+}
 //    @GetMapping("/products/{category}/{p1}/{p2}")
 //    public List<Products> getNodeByPriceRange(@PathVariable(value="category")String category,@PathVariable(value="p1")String p1,@PathVariable(value="p2")String p2)
 //    {
